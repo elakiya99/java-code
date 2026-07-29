60784f5f01bdb88e3035ed67cdc0ec0af3ffa0c9
elakiya99 <23bm012@excelcolleges.com>
2026-07-29 15:54:44 +0530

    Add heart pattern printing program in Java
    This program prompts the user for a size and prints a heart pattern using asterisks based on the input size.

---
+++ Heart pattern.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main{
+    public static void main (String []args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter size:");
+        int n = sc.nextInt();
+        for(int i=(n/2)+1;i<=n;i+=2){
+            for (int j=1;j<n-i;j+=2)
+            System.out.print(" ");
+            for (int j=1; j<=i;j++)
+            System.out.print("*");
+            for (int j=1;j<=n-i;j++)
+            System.out.print(" ");
+            for (int j=1;j<=i;j++)
+            System.out.print("*");
+            System.out.println();
+        }
+        for (int i=n;i>=1;i--){
+            for (int j=i;j<n;j++)
+            System.out.print(" ");
+            for(int j=i;j<=(2*i-1);j++)
+            System.out.print("*");
+            System.out.println();
+        }
+         sc.close();
+}
+}
