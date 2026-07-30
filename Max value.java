0eb30e2b2ecc4417748a02e6614bf41d9a6745e5
elakiya99 <23bm012@excelcolleges.com>
2026-07-30 16:12:44 +0530

    Create Max value.java

---
+++ Max value.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main (String[] args){
+     int a = 15;
+     int b = 5;
+     int c = 25;
+     
+     if(a>b && a>c){
+          System.out.println("a is greater");
+     }
+          else if (b>a && b>c){
+              System.out.println("b is greater");
+          }
+          else {
+              System.out.println("c is greater");
+          }
+    }
+    }
+     
