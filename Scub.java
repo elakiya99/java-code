ea393aa80f944f9b41f1e24e612f413fd5c24350
elakiya99 <23bm012@excelcolleges.com>
2026-07-30 18:22:35 +0530

    Create Scub.java

---
+++ Scub.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Main{
+    public static void main(String [] args){
+        Scanner sc=new Scanner (System.in);
+            System.out.print("Enter n:");
+            int n = sc.nextInt();
+            for (int i=1;i<=n;i++){
+                for (int j=1;j<=n;j++){
+                    if (i==1|| i==n || j==1|| j==n){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+             for (int i=1;i<=n;i++){
+                 System.out.print("  ");
+                for (int j=1;j<=n;j++){
+                    if (i==1|| i==n || j==1|| j==n){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+            
+            }
+}
